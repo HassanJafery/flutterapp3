6a1c97be51568807c590b14b58739dff624622f3
HassanJafery <mhassanrazajaffery@gmail.com>
2024-10-23 15:58:45 +0500

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,37 +1,69 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text("bar buttons")),
-      body: Center(
-          child: ButtonBar(
-        alignment: MainAxisAlignment.center,
-        buttonPadding: EdgeInsets.symmetric(horizontal: 10),
-        children: [
+      home: Scaffold(
+        appBar: AppBar(title: Text('Flutter Form Example')),
+        body: MyCustomForm(),
+      ),
+    );
+  }
+}
+
+class MyCustomForm extends StatefulWidget {
+  @override
+  MyCustomFormState createState() => MyCustomFormState();
+}
+
+class MyCustomFormState extends State<MyCustomForm> {
+  final _formKey = GlobalKey<FormState>();
+  String? _name;
+  String? _email;
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('Name: $_name');
+      print('Email: $_email');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Name'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+            onSaved: (value) => _name = value,
+          ),
+          TextFormField(
+            decoration: InputDecoration(labelText: 'Email'),
+            validator: (value) {
+              if (value == null || value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+            onSaved: (value) => _email = value,
+          ),
           ElevatedButton(
-              onPressed: () {
-                print('Accepted');
-              },
-              child: Text("Accept")),
-          ElevatedButton(
-              onPressed: () {
-                print('Declined');
-              },
-              child: Text('Decline')),
-          ElevatedButton(
-              onPressed: () {
-                print('Later clicked');
-              },
-              child: Text('Later'))
+            onPressed: _submitForm,
+            child: Text('Submit'),
+          ),
         ],
-      )),
-    ));
+      ),
+    );
   }
 }
